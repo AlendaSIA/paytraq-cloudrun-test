14de03e80439485df08cbfd8fedb903f75c9a6c1
AlendaSIA <alenda.jurmala@gmail.com>
2025-03-21 15:15:55 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,16 @@
-import os
-from flask import Flask, jsonify
+from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello():
+@app.route("/", methods=["GET"])
+def index():
     return jsonify({"message": "Sveiki no Cloud Run testa aplikācijas!"})
 
-if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 8080))
-    app.run(debug=False, host='0.0.0.0', port=port)
+@app.route("/paytraq-webhook", methods=["POST"])
+def paytraq_webhook():
+    data = request.json
+    print("Saņemts dati no PayTraq:", data)
+    return jsonify({"status": "dati saņemti"}), 200
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8080)
